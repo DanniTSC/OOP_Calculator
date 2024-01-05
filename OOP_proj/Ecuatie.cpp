a682b51a06443c56bfb349e2a8dbc9f581f7b9d2
Tascu_Daniel <tascudaniel22@stud.ase.ro>
2024-01-05 18:10:03 +0200

    Meniu faza 2

--- OOP_proj/Ecuatie.cpp
+++ OOP_proj/Ecuatie.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<string>
 #include<cmath>
+#include<fstream>
 
 using namespace std;
 
--- OOP_proj/Source.cpp
+++ OOP_proj/Source.cpp
@@ -1,64 +1,123 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 #include "Calcul.h"
 #include "Descompunere.h"
 #include "Ecuatie.h"
 #include<iomanip>
 using namespace std;
 
+double proceseazaExpresie(const string& expresie)
+{
+    double numere[100];
+    char operatori[100];
+    int nrNumere = 0, nrOperatori = 0;
+
+    Descompunere::descompunereExpresie(expresie, numere, operatori, nrNumere, nrOperatori);
+
+    for (int i = 0; i < nrNumere; i++) {
+        cout << "numar: " << numere[i] << endl;
+    }
+
+    for (int i = 0; i < nrOperatori; i++) {
+        cout << "operator: " << operatori[i] << endl;
+    }
+
+    double rezultat = Calcul::efectuareOperatie(numere, operatori, nrNumere, nrOperatori);
+    return rezultat;
+}
+
+void citireEcuatiiDinFisier()
+{
+    string numeFisier;
+    cout << "introduceti nume fisier:";
+    cin >> numeFisier;
+
+    ifstream fisierIn(numeFisier);
+    if (!fisierIn) {
+        cout << "fisierul s-a deschis" << endl;
+        return;
+    }
+    
+    string linie;
+    while (getline(fisierIn, linie))
+    {
+        try
+        {
+            double rezultat = proceseazaExpresie(linie);
+            cout << "rezultatul pentru ecuatia : " << linie << ' ' << rezultat << endl;
+        }
+        catch (const exception& e)
+        {
+            cout << "Eroare la procesarea ecuatiei: " << linie << ' ' << e.what() << endl;
+        }
+    }
+    fisierIn.close();
+    cout << "Ecuatii citite din fisier" << endl;
+}
+
+void salvareRezultateInFisier(double rezultat)
+{
+    string numeFisier;
+    cout << "introduceti numele fisierului unde se va salva";
+    cin >> numeFisier;
+
+    ofstream fisierOut(numeFisier, ios::binary);
+    
+    if (!fisierOut)
+    {
+        cout << "Fisierul nu a putut fi deschis pentru scriere." << endl;
+        return;
+    }
+
+    fisierOut.write(reinterpret_cast<const char*>(&rezultat), sizeof(rezultat));
+    fisierOut.close();
+    cout << "rezultat salvar in fisier" << endl;
+}
+
+
+
+
+
 int main() {
-    //string expresie;
-    //getline(cin, expresie);
+    double intermediar = 0;
 
-    //double numere[100];
-    //char operatori[100];
-    //int nrNumere = 0, nrOperatori = 0;
-
-    //Descompunere::descompunereExpresie(expresie, numere, operatori, nrNumere, nrOperatori);
-
-    //for (int i = 0; i < nrNumere; i++) {
-    //    cout << "numar: " << numere[i] << endl;
-    //}
-
-    //for (int i = 0; i < nrOperatori; i++) {
-    //    cout << "operator: " << operatori[i] << endl;
-    //}
-
-    //double rezultat = Calcul::efectuareOperatie(numere, operatori, nrNumere, nrOperatori);
-    //
-    ////fixed este pentru 24343243.0002 - 3 
-    //cout << fixed << setprecision(4) << "rezultatul este: " << rezultat << endl;
-
-    while (true) {
+        while (true) 
+        {
         cout << "Meniu:" << endl;
-        cout << "1. Introducere Ecuatie" << endl;
+        cout << "1. Introducere Ecuatie si rezolvare" << endl;
         cout << "2. Citire ecuatie din fisier" << endl;
         cout << "3. Salvare rezultat intermediar in fisier" << endl;
         cout << "4. Exit" << endl;
 
-        int alegMeniu = 0;
+        int alegMeniu;
         cout << "Ce optiune alegeti ?";
         cin >> alegMeniu;
 
-        switch (alegMeniu)
-        {
-        case 1:
-            //ecuatie cod
+        switch (alegMeniu){
+        case 1: {
+            string expresie;
+            cout << "Introduceti ecuatia: ";
+            cin.ignore();
+            getline(cin, expresie);
+
+            cout << fixed << setprecision(4) << "rezultatul este: " << proceseazaExpresie(expresie) << endl;
             break;
+        }
         case 2:
-            //cod ecuatie fisier
+            citireEcuatiiDinFisier();
         break;
         case 3:
-            //salvare rez in fisier
+            salvareRezultateInFisier(intermediar);
         break;
         case 4:
-            cout << "iesire din program" << endl;
+            cout << "Iesire din program" << endl;
             return 0;
         default:
             cout << "Momentan optiune invalida. Incercati din nou." << endl;
 
+            }
         }
-    }
 
     return 0;
 }
